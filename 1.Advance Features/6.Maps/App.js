45a5a4e79246204927bf44b6eb2a90b4278f5f23
adan <adanaligakhar@gmail.com>
2022-05-04 00:25:10 +0500

    more about maps

--- 1.Advance Features/6.Maps/App.js
+++ 1.Advance Features/6.Maps/App.js
@@ -20,3 +20,75 @@ const CURRENCY_MAP = {
     Pakistan : 'Rupee',
 }
 
+
+// in Map when we loop it will loop in order, which is not in the case of object according to Kayle
+// also it is harder to map a object see below after map3 see map 4
+const map3 = new Map([
+    [1, 'A'],
+    [2, 'B'],
+    [3, 'C'],
+    [4, 'D'],
+])
+
+
+map3.forEach((value,key) => console.log(value,key))
+
+const obj = {
+    1 : 'A',
+    2 : 'B',
+    3 : 'C',
+    4  : 'D',
+}
+
+
+
+Object.entries(obj).forEach(([key,value]) => {
+    console.log(key,value)
+})
+
+
+
+
+//map also have size property
+console.log(map3.size)
+
+
+
+const map4 = new Map([
+    [1, 'A'],
+    [2, 'B'],
+    [3, 'C'],
+    [4, 'D'],
+])
+
+map4.set(2,'eden') //map method
+
+console.log(map4.get(2)) //these are methods of maps
+
+console.log(map4.has(4)) // if 4 exist in map
+
+map4.delete(1);
+
+console.log(map4)
+
+
+let obj2 = {
+    name : 'adan'
+    
+}
+
+
+const map5 = new Map([
+    [1, {id : 1, name : 'adan', description : 'desc'}],
+    [2, {id : 2, name : 'ali', description : 'desc'}],
+    [3, {id : 3, name : 'raja', description : 'desc'}],
+]);
+
+
+function getItem(id){
+    
+   return map5.get(id)
+
+}
+
+console.log(getItem(1))
